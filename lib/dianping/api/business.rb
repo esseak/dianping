3adab9f10c0a199b6702dd5d4a0e752ccbeab6b0
libiao <kkxlkkxllb@gmail.com>
2013-04-26 15:02:37 +0800

    fix

--- lib/dianping/api/business.rb
+++ lib/dianping/api/business.rb
@@ -5,7 +5,7 @@ module DianPing
       include DianPing::API::Utils
       # 搜索商户
       def find_businesses(params={'city' => DianPing::API::DFT_CITY})
-        assert(params.is_a?(Hash))
+        #assert(params.is_a?(Hash))
         #assert(params['city'] || (params['longitude'] && params['latitude']), '请求参数组合无效: city与longitude/latitude至少选其一')
         path = "/v1/business/find_businesses"
         hash, resp = get(path, params, {'default_content_hash' => {'businesses' => [], 'count' => 0}})
@@ -14,7 +14,7 @@ module DianPing
 
       # 获取指定商户信息
       def get_single_business(params={})
-        assert(params['business_id'] && params['business_id'].is_a?(Fixnum))
+        #assert(params['business_id'] && params['business_id'].is_a?(Fixnum))
         path = "/v1/business/get_single_business"
         hash, resp = get(path, params)
         return hash
@@ -22,7 +22,7 @@ module DianPing
 
       # 获取商户月热搜词
       def get_monthly_trends(params={'city' => DianPing::API::DFT_CITY})
-        assert(params['category'])
+        #assert(params['category'])
         path = "/v1/business/get_monthly_trends"
         hash, resp = get(path, params, {'default_content_hash' => {'trends' => [], 'count' => 0}})
         return hash
